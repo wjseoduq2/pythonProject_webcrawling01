834ca25e03ae7c8a0ba3f35eb99d89fda839efc6
wjseoduq2 <dragon65@naver.com>
2024-04-01 14:24:47 +0900

    1'st change

--- weatherCrawling.py
+++ weatherCrawling.py
@@ -1,4 +1,9 @@
-import sys
+import requests  # pip install requests
 
 # https://search.naver.com/search.naver?&query=한남동날씨
 
+from bs4 import BeautifulSoup  # pip install beautifulsoup4
+
+weatherHtml = requests.get("https://search.naver.com/search.naver?&query=한남동날씨")
+# https://search.naver.com/search.naver?&query=한남동날씨
+print(weatherHtml.text)
