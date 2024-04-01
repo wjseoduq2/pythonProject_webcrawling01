0df0efc59d4d039609d6027087adfe059ba9acc5
wjseoduq2 <dragon65@naver.com>
2024-04-01 15:47:49 +0900

    2'nd change

--- weatherCrawling.py
+++ weatherCrawling.py
@@ -14,9 +14,30 @@ print(weatherSoup)
 areaText = weatherSoup.find("h2", {"class":"title"}).text  # tag 필요없고 text만 뽑아냄
 # 날씨 지역 이름 가져오기
 areaText = areaText.strip()
-# print(areaText)
+print(areaText)
 
 todayTempText = weatherSoup.find("div", {"class":"temperature_text"}).text
 todayTempText = todayTempText[6:11].strip()
-# print(todayTempText)
+print(todayTempText)
 # print(todayTempText[6:11])
+
+yesterdayTempText = weatherSoup.find("span", {"class":"temperature up"}).text
+yesterdayTempText = yesterdayTempText.strip()
+print(yesterdayTempText)
+
+todayWeatherText = weatherSoup.find("span", {"class":"weather before_slash"}).text
+todayWeatherText = todayWeatherText.strip()
+print(todayWeatherText)
+
+senseTempText = weatherSoup.find("dd", {"class":"desc"}).text
+senseTempText = senseTempText.strip()
+print(senseTempText)
+
+todayInfoText = weatherSoup.select("ul.today_chart_list>li")  # 미세먼지, 초미세먼지, 자외선, 일몰 모두 갖고 옴
+# print(todayInfoText[0:2])
+dustInfo = todayInfoText[0].find("span", {"class":"txt"}).text
+dustInfo = dustInfo.strip()
+print(dustInfo)
+superdustInfo = todayInfoText[1].find("span", {"class":"txt"}).text
+superdustInfo = superdustInfo.strip()
+print(superdustInfo)
