aed3b6bd6f531315689338ad4b559d50345f2435
wjseoduq2 <dragon65@naver.com>
2024-04-01 15:00:10 +0900

    2'nd change

--- weatherCrawling.py
+++ weatherCrawling.py
@@ -7,3 +7,16 @@ from bs4 import BeautifulSoup  # pip install beautifulsoup4
 weatherHtml = requests.get("https://search.naver.com/search.naver?&query=한남동날씨")
 # https://search.naver.com/search.naver?&query=한남동날씨
 print(weatherHtml.text)
+
+weatherSoup = BeautifulSoup(weatherHtml.text, 'html.parser')
+print(weatherSoup)
+
+areaText = weatherSoup.find("h2", {"class":"title"}).text  # tag 필요없고 text만 뽑아냄
+# 날씨 지역 이름 가져오기
+areaText = areaText.strip()
+# print(areaText)
+
+todayTempText = weatherSoup.find("div", {"class":"temperature_text"}).text
+todayTempText = todayTempText[6:11].strip()
+# print(todayTempText)
+# print(todayTempText[6:11])
